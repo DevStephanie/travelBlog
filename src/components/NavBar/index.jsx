aef68e8952d6bf6a4e24d970a80337696de07c22
Stephanie Abdullah <stephabdullah@gmail.com>
2023-06-23 02:22:29 -0500

    created my index.jsx in my travelItem folder

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './NavBar/styles.css';
+
+const NavBar = ({ formSubmit, handleSearchKey, clearSearch}) => ( 
+    <div className="searchBar-wrap"> 
+    <form onSubmit={formSubmit}> 
+        <input 
+            type ='text'
+            placeholder="Search by Category"
+            value={value}
+            onChange={handleSearchKey}/>
+    
+    {value && <span onClick={clearSearch}>X</span>}
+    <button> GO</button>
+    </form>
+    
+    </div>
+);
+
+export default SearchBar; 
