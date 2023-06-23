ad974c22b5a9f05f06de60ecba444fc918417d18
Stephanie Abdullah <stephabdullah@gmail.com>
2023-06-22 23:55:36 -0500

    inital commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from './Travel.jsx';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Travel link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Travel link/i);
   expect(linkElement).toBeInTheDocument();
 });
